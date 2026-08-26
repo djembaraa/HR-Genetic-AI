2e806de190035e6168299eb70609ba3c60138856
djembar <djembararafat98@gmail.com>
2026-08-26 11:38:04 +0700

    feat: enhance error response in middleware and update Redis service configuration in Docker Compose

--- backend-node/index.js
+++ backend-node/index.js
@@ -174,7 +174,7 @@ app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500).json({ error: 'Internal Server Error', msg: err.message, stack: err.stack });
 });
 
 // Start BullMQ Worker
